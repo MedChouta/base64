7cc1474247a3b15971d0c775605bb226956b8ce3
mohammed chouta <mohammedchouta10@gmail.com>
2019-07-27 16:40:23 +0100

    Added file support
    Can now encode/decode files

--- base64.go
+++ base64.go
@@ -1,8 +1,10 @@
+
 package main
 
 import (
 	b64 "encoding/base64"
 	"os"
+	"io/ioutil"
 	"fmt"
 	"strings"
 )
@@ -19,8 +21,58 @@ func decode(input string) string{
 	return string(inputDec)
 }
 
+func checkError(e error) bool{
+	if e != nil{
+		panic(e)
+		return false
+	}
+
+	return true
+}
+
+func EDfile(file string, option int) bool{
+
+	inputData, err := ioutil.ReadFile(file)
+	state := checkError(err)
+
+	newFileName := strings.Join(strings.Split(file, "."), "_O.")
+	outputFile, err := os.Create(newFileName)
+	state = checkError(err)
+
+
+	/*
+	option 1: encode
+	option 2: decode
+	*/
+							
+	var outputData []byte
+
+	if option == 1{
+		outputData = []byte(encode(string(inputData)))
+	}else if option == 2{
+		outputData = []byte(decode(string(inputData)))
+	}
+
+	err = ioutil.WriteFile(newFileName, outputData, 0644)
+
+	state = checkError(err)
+
+	defer outputFile.Close()
+
+	return state
+
+}
+
 func showHelp() string{
-	return "Usage: \n\tbase64 [option] string \nOptions: \n\t-e(--encode): Encodes the string\n\t-d(--decode): Decodes the string"
+	return `
+Usage: 
+	base64 [option] string or file 
+Options: 
+	-e(--encode): Encodes the string
+	-d(--decode): Decodes the string
+	-Ef(--encodeFile): Encodes file
+	-Df(--decodeFile): Decodes file
+`
 }
 
 func main(){
@@ -36,6 +88,20 @@ func main(){
 		} else if option == "-e" || option == "--encode"{
 			encString := encode(input)
 			fmt.Print(encString)
+		}else if option == "-Ef" || option == "--encodeFile"{
+			isSuccessful := EDfile(input, 1)
+			if isSuccessful{
+				fmt.Print("Encoding successful")
+			}else{
+				fmt.Print("An error has occured")
+			}
+		}else if option == "-Df" || option == "--decodeFile" {
+			isSuccessful := EDfile(input, 2)
+			if isSuccessful{
+				fmt.Print("Decoding successful")
+			}else{
+				fmt.Print("An error has occured")
+			}
 		}else{
 			fmt.Print(showHelp())
 		}
